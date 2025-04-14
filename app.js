2507a8aebf48c1dc342669835a41bb1dc1145164
maeda-takumi <jiangqiantian54@gmail.com>
2025-04-14 16:39:11 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const puppeteer = require('puppeteer-core');  // puppeteer-coreを使用
 (async () => {
   const browser = await puppeteer.launch({
     headless: true,
-    executablePath: '/tmp/chrome-linux64/chrome',  // /tmpディレクトリ内のChromeを使用
-    args: ['--no-sandbox', '--disable-dev-shm-usage']  // 必要な引数
+    executablePath: '/tmp/chrome',  // 修正: /tmp/chrome を指定
+    args: ['--no-sandbox', '--disable-dev-shm-usage']
   });
 
   const page = await browser.newPage();
   await page.goto('https://www.google.com');
-  console.log(await page.title());  // Googleのタイトルを出力
+  console.log(await page.title());
   await browser.close();
 })();
